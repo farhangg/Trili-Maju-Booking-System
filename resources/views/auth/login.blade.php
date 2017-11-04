792941b87b77411260baad5fea5c59458141d79e
harixth <harixth@yahoo.com>
2017-11-04 14:39:02 +0800

    Modify login page
    login page template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,151 @@
-@extends('layouts.app')
+<link href="{{ asset('demo/default/base/style.bundle.css') }}" rel="stylesheet">
+<link href="{{ asset('vendors/base/vendors.bundle.css') }}" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js"></script>
+<script>
+          WebFont.load({
+            google: {"families":["Poppins:300,400,500,600,700","Roboto:300,400,500,600,700"]},
+            active: function() {
+                sessionStorage.fonts = true;
+            }
+          });
+</script>
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome to Trilli Wagons Booking System</div>
-
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+<body class="m--skin- m-header--fixed m-header--fixed-mobile m-aside-left--enabled m-aside-left--skin-dark m-aside-left--offcanvas m-footer--push m-aside--offcanvas-default"  >
+<!-- begin:: Page -->
+		<div class="m-grid m-grid--hor m-grid--root m-page">
+			<div class="m-grid__item m-grid__item--fluid m-grid m-grid--hor m-login m-login--singin m-login--2 m-login-2--skin-2" id="m_login" style="background-image: url(/app/media/img/bg/railway-image.jpg);">
+				<div class="m-grid__item m-grid__item--fluid	m-login__wrapper">
+					<div class="m-login__container">
+						<div class="m-login__logo">
+							<a href="#">
+								<img src="/app/media/img/logos/trili-logo.png">
+							</a>
+						</div>
+						<div class="m-login__signin">
+							<div class="m-login__head">
+								<h1 style="color:#c4c5d6;text-align: center;font-weight: bold;text-shadow:-1px -1px 0 #000,1px -1px 0 #000,-1px 1px 0 #000,1px 1px 0 #000;">
+									Trili Maju <br> Chartered Train System
+								</h1>
+							</div>
+							<form class="m-login__form m-form" action="">
+								<div class="form-group m-form__group">
+									<input class="form-control m-input"   type="text" placeholder="Email" name="email" autocomplete="off">
+								</div>
+								<div class="form-group m-form__group">
+									<input class="form-control m-input m-login__form-input--last" type="password" placeholder="Password" name="password">
+								</div>
+								<div class="row m-login__form-sub">
+									<div class="col m--align-left m-login__form-left">
+										<label class="m-checkbox  m-checkbox--focus">
+											<input type="checkbox" name="remember">
+											Remember me
+											<span></span>
+										</label>
+									</div>
+									<div class="col m--align-right m-login__form-right">
+										<a href="javascript:;" id="m_login_forget_password" class="m-link">
+											Forget Password ?
+										</a>
+									</div>
+								</div>
+								<div class="m-login__form-action">
+									<button id="m_login_signin_submit" class="btn m-btn--pill m-btn--air btn-lg btn-outline-metal m-btn m-btn--custom m-login__btn m-login__btn--primary">
+											Sign In
+									</button>
+								</div>
+							</form>
+						</div>
+						<div class="m-login__signup">
+							<div class="m-login__head">
+								<h3 class="m-login__title">
+									Sign Up
+								</h3>
+								<div class="m-login__desc">
+									Enter your details to create your account:
+								</div>
+							</div>
+							<form class="m-login__form m-form" action="">
+								<div class="form-group m-form__group">
+									<input class="form-control m-input" type="text" placeholder="Fullname" name="fullname" >
+								</div>
+								<div class="form-group m-form__group">
+									<input class="form-control m-input" type="text" placeholder="Email" name="email" autocomplete="off">
+								</div>
+								<div class="form-group m-form__group">
+									<input class="form-control m-input" type="password" placeholder="Password" name="password">
+								</div>
+								<div class="form-group m-form__group">
+									<input class="form-control m-input m-login__form-input--last" type="password" placeholder="Confirm Password" name="rpassword">
+								</div>
+								<div class="row form-group m-form__group m-login__form-sub">
+									<div class="col m--align-left">
+										<label class="m-checkbox m-checkbox--focus">
+											<input type="checkbox" name="agree">
+											I Agree the
+											<a href="#" class="m-link m-link--focus">
+												terms and conditions
+											</a>
+											.
+											<span></span>
+										</label>
+										<span class="m-form__help"></span>
+									</div>
+								</div>
+								<div class="m-login__form-action">
+									<button id="m_login_signup_submit" class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air  m-login__btn">
+										Sign Up
+									</button>
+									&nbsp;&nbsp;
+									<button id="m_login_signup_cancel" class="btn btn-outline-focus m-btn m-btn--pill m-btn--custom  m-login__btn">
+										Cancel
+									</button>
+								</div>
+							</form>
+						</div>
+						<div class="m-login__forget-password">
+							<div class="m-login__head">
+								<h3 class="m-login__title">
+									Forgotten Password ?
+								</h3>
+								<div class="m-login__desc">
+									Enter your email to reset your password:
+								</div>
+							</div>
+							<form class="m-login__form m-form" action="">
+								<div class="form-group m-form__group">
+									<input class="form-control m-input" type="text" placeholder="Email" name="email" id="m_email" autocomplete="off">
+								</div>
+								<div class="m-login__form-action">
+									<button id="m_login_forget_password_submit" class="btn btn-focus m-btn m-btn--pill m-btn--custom m-btn--air  m-login__btn m-login__btn--primaryr">
+										Request
+									</button>
+									&nbsp;&nbsp;
+									<button id="m_login_forget_password_cancel" class="btn btn-outline-focus m-btn m-btn--pill m-btn--custom m-login__btn">
+										Cancel
+									</button>
+								</div>
+							</form>
+						</div>
+<!-- 						<div class="m-login__account"> -->
+<!-- 							<span class="m-login__account-msg"> -->
+<!-- 								Don't have an account yet ? -->
+<!-- 							</span> -->
+<!-- 							&nbsp;&nbsp; -->
+<!-- 							<a href="javascript:;" id="m_login_signup" class="m-link m-link--light m-login__account-link"> -->
+<!-- 								Sign Up -->
+<!-- 							</a> -->
+<!-- 						</div> -->
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- end:: Page -->
+    	<!--begin::Base Scripts -->
+		<script src="{{ asset('vendors/base/vendors.bundle.js') }}" type="text/javascript"></script>
+		<script src="{{ asset('demo/default/base/scripts.bundle.js') }}" type="text/javascript"></script>
+		<!--end::Base Scripts -->   
+        <!--begin::Page Snippets -->
+		<script src="{{ asset('snippets/pages/user/login.js') }}" type="text/javascript"></script>
+		<!--end::Page Snippets -->
+		</body>
+	<!-- end::Body -->
