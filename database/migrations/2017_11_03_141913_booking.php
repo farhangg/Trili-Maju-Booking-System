5294a97ded563123107917c065a436b96c6866bd
Ahmad Rusyaidi <rusyaidijumat@yahoo.com>
2017-11-04 14:41:42 +0800

    Add Data Entry for Booking Form
    -Booking form db with schema
    -Insert blum buat

---
+++ database/migrations/2017_11_03_141913_booking.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Booking extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::create('booking', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('CompanyName');
+            $table->string('Container20');
+            $table->string('Container40');
+            $table->string('TotalContainer');
+            $table->string('BookingReference');
+            $table->string('Commodity');
+            $table->string('Liner');
+            $table->string('VesselName');
+            $table->date('VesselETA');
+            $table->time('ClosingTime');
+            $table->date('ChosenDate');
+            $table->string('PickUpDepot');
+            $table->string('ContainerNo');
+            $table->string('Remarks');
+            $table->string('Status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('booking');
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,43 +29,129 @@
                         <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="/booking/insert">
                         {{ csrf_field() }}
-                    <div>
+
+                        <div style='padding-bottom: 50px;'>
+                            <label class="col-md-4 control-label">Company Name*</label>
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="CompanyName">
+                            </div>
+                        </div>
+
+                        <div style='padding-bottom: 50px;'>
+                            <label class="col-md-4 control-label">Booking Reference*</label>
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="BookingReference">
+                            </div>
+                        </div>
+
+                        <div style='padding-bottom: 50px;'>
                             <label for="container20" class="col-md-4 control-label">20-ft Container</label>
 
                             <!--{{ Auth::user()->name }} -->
 
                             <div class="col-md-6">
-                                <input id="email" class="form-control" name="container20">
+                                <input type="number" class="form-control" name="container20">
                             </div>
-                            </div>
+                        </div>
   
                        <div style='padding-bottom: 50px;'>
                             <label class="col-md-4 control-label">40-ft Container</label>
                             <div class="col-md-6">
-                                <input type="container20" class="form-control" name="container40">
+                                <input type="number" class="form-control" name="container40">
                             </div>
                         </div>
 
-                         <div>
-                            <label class="col-md-4 control-label">Total</label>
+                        <div style='padding-bottom: 50px;'>
+                            <label class="col-md-4 control-label">Total Container</label>
                             <div class="col-md-6">
-                                <input class="form-control" name="containerTotal">
+
                             </div>
                         </div>
 
-                         <div>
+                        <div style='padding-bottom: 50px;'>
+                            <label class="col-md-4 control-label">Commodity*</label>
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="Commodity">
+                            </div>
+                        </div>
+
+                        <div style='padding-bottom: 50px;'>
+                            <label class="col-md-4 control-label">Liner*</label>
+                            <div class="col-md-6">
+                                <select class="form-control" name="Liner">
+                                   <option value="test">test</option>
+                                   <option value="test">test</option>
+                                  <option value="test">test</option>
+                                  <option value="test">test</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div style='padding-bottom: 50px;'>
+                            <label class="col-md-4 control-label">Vessel Name*</label>
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="VesselName">
+                            </div>
+                        </div>
+
+                        <div style='padding-bottom: 50px;'>
+                            <label class="col-md-4 control-label">Vessel ETA PEN*</label>
+                            <div class="col-md-6">
+                                <input type="date" class="form-control" name="VesselETA">
+                            </div>
+                        </div>
+
+                        <div style='padding-bottom: 50px;'>
+                            <label class="col-md-4 control-label">Closing Time*</label>
+                            <div class="col-md-6">
+                                 <input type="time" name="ClosingTime">
+                            </div>
+                        </div>
+
+                         <div style='padding-bottom: 50px;'>
                             <label class="col-md-4 control-label">Date</label>
                             <div class="col-md-6">
-                                 <input type="date" name="chosenDate">
+                                 <input type="date" name="ChosenDate">
                                 <button value="checkAvailability">Check Availability </button>
                             </div>
+                        </div>
 
+                        <div style='padding-bottom: 50px;'>
+                            <label class="col-md-4 control-label">Pick-up Depot*</label>
+                            <div class="col-md-6">
+                                <select class="form-control" name="PickUpDepot">
+                                   <option value="test">test</option>
+                                   <option value="test">test</option>
+                                  <option value="test">test</option>
+                                  <option value="test">test</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div style='padding-bottom: 50px;'>
+                            <label class="col-md-4 control-label">Container No</label>
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="ContainerNo">
+                            </div>
+                        </div>
+
+                        <div style='padding-bottom: 100px;'>
+                            <label class="col-md-4 control-label">Remarks</label>
+                            <div class="col-md-6">
+                                <textarea class="form-control" name="Remarks"></textarea>
+                            </div>
                         </div>
 
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Book
+                                    Submit
+                                </button> 
+                                <button type="Clear" class="btn btn-primary">
+                                    Clear Form
+                                </button> 
+                                <button type="Print" class="btn btn-primary">
+                                    Print
                                 </button> 
                             </div>
                         </div>
